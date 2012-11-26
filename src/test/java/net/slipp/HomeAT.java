1bc52da8daf6d3613c1c60d09f0c82c4e5f11cc8
javajigi <javajigi@gmail.com>
2012-11-26 18:11:11 +0900

    #3: 회원 가입을 할 수 있다. 메인 페이지에 대한 Acceptance Test를 추가함

---
+++ src/test/java/net/slipp/HomeAT.java
@@ -0,0 +1,26 @@
+package net.slipp;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HomeAT {
+	private WebDriver driver;
+
+	@Before
+	public void setup() {
+		System.setProperty("webdriver.firefox.bin", "D:\\tools\\firefox\\firefox.exe");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void index() throws Exception {
+		driver.get("http://localhost:8080");
+		assertThat(driver.getTitle(), is("SLiPP"));
+	}
+}
