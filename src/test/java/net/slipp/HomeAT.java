68661fb4285f46aba8504d58fe2d2993d2931754
javajigi <javajigi@gmail.com>
2012-11-26 18:22:02 +0900

    #3: 회원 가입을 할 수 있다. webapp 경로 변경 및 자동화된 테스트 추가함.

--- src/test/java/net/slipp/HomeAT.java
+++ src/test/java/net/slipp/HomeAT.java
@@ -3,6 +3,7 @@ package net.slipp;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -22,5 +23,10 @@ public class HomeAT {
 	public void index() throws Exception {
 		driver.get("http://localhost:8080");
 		assertThat(driver.getTitle(), is("SLiPP"));
+	}
+	
+	@After
+	public void teardown() {
+		driver.quit();
 	}
 }
