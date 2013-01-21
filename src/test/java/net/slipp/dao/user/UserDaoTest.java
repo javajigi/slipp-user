3ad13aebab48fee1039142083bbc7696ff983631
hang1416 <wlswls82>
2013-01-21 19:15:59 +0900

    단위테스트 @Before 추가

--- src/test/java/net/slipp/dao/user/UserDaoTest.java
+++ src/test/java/net/slipp/dao/user/UserDaoTest.java
@@ -11,29 +11,36 @@ import net.slipp.factory.DaoFactory;
 import net.slipp.support.jdbc.ConnectionManager;
 import net.slipp.support.jdbc.MySQLConnectionManagerImpl;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserDaoTest {
+	User expectedUser;
+	UserDao userDao;
+	
+	@Before
+	public void setup(){
+		expectedUser = new User("userId", "password", "name", "javajigi@email.com");
+	}
+	
 	@Test
 	public void crud() throws Exception {
-		User expected = new User("userId", "password", "name", "javajigi@email.com");
 		 
-		UserDao userDao = new TestUserDaoImpl();
-		userDao.insert(expected);
+		userDao = new TestUserDaoImpl();
+		userDao.insert(expectedUser);
 		
-		User actual = userDao.findByUserId(expected.getUserId());
-		assertThat(actual, is(expected));
+		User actual = userDao.findByUserId(expectedUser.getUserId());
+		assertThat(actual, is(expectedUser));
 	}
 	
 	@Test
 	public void crudH2() throws Exception {
-		User expected = new User("userId", "password", "name", "javajigi@email.com");
 	
-		UserDao userDao = DaoFactory.getUserDao();
+		userDao = DaoFactory.getUserDao();
 		userDao.deleteAllUser();
-		userDao.insert(expected);
+		userDao.insert(expectedUser);
 	
-		User actual = userDao.findByUserId(expected.getUserId());
-		assertThat(actual, is(expected));
+		User actual = userDao.findByUserId(expectedUser.getUserId());
+		assertThat(actual, is(expectedUser));
 	}
 }
