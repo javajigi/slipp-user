cbcaf2a75d945a9c84ec2e5ef10729893ee9bd49
hang1416 <wlswls82>
2013-01-22 19:06:30 +0900

    STEP4

--- src/main/java/net/slipp/dao/user/UserDaoImpl.java
+++ src/main/java/net/slipp/dao/user/UserDaoImpl.java
@@ -10,91 +10,29 @@ import net.slipp.domain.user.User;
 import net.slipp.support.jdbc.ConnectionManager;
 
 public class UserDaoImpl implements UserDao{
-	private ConnectionManager connectionManager = null;
-	private  static final String QUERY_TYPE_DELETE ="DELETE";
-	private  static final String QUERY_TYPE_SELECT ="SELECT";
-	private  static final String QUERY_TYPE_INSET ="INSERT";
+	ConnectionManager connectionManager;
 	
 	public UserDaoImpl (ConnectionManager connectionManager) {
 		this.connectionManager = connectionManager;
 	}
 	
 	public void insert(User user) throws SQLException, PropertyVetoException {
+		JdbcTemplate template = new JdbcTemplate(connectionManager);
 		String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-		execute(user, sql, QUERY_TYPE_INSET);
+		template.execute(user, sql, "INSERT");
 	}
 
-	
-	private User execute(User user,String sql, String queryType) throws SQLException,
-	            PropertyVetoException {
-	         
-	        Connection con = null;
-	        PreparedStatement pstmt = null;
-	        ResultSet rs = null;
-	         
-	        try {
-	            con = connectionManager.getConnection();
-	             
-	            pstmt = con.prepareStatement(sql);
-	            if(!QUERY_TYPE_DELETE.equals(queryType)){
-	                setPreparedStatement(user, pstmt, queryType);
-	            }
-	            if(QUERY_TYPE_SELECT.equals(queryType)){
-	                rs = pstmt.executeQuery();
-	                return setResultSet(rs);
-	            }else{
-	                pstmt.executeUpdate();
-	                return null;
-	            }
-	        } finally {
-	            close(con, pstmt);
-	            if (rs != null) {
-	            	rs.close();
-	    		}
-	        }
-	    }
-	    private User setResultSet(ResultSet rs) throws SQLException {
-	        User rsUser = null;
-	        if (rs.next()) {
-	            rsUser = new User(
-	                    rs.getString("userId"), 
-	                    rs.getString("password"), 
-	                    rs.getString("name"),
-	                    rs.getString("email"));
-	        }
-	        return rsUser;
-	    }
-	    private void setPreparedStatement(User user, PreparedStatement pstmt, String queryType)
-	            throws SQLException {       
-	         
-	        pstmt.setString(1, user.getUserId());
-	        if(QUERY_TYPE_INSET.equals(queryType)){
-	            pstmt.setString(2, user.getPassword());
-	            pstmt.setString(3, user.getName());
-	            pstmt.setString(4, user.getEmail());
-	        }
-	    }
-
 	public User findByUserId(String userId) throws SQLException, PropertyVetoException {
-		
-			String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-			User user = new User(userId,"","","");
-			return execute(user,sql,QUERY_TYPE_SELECT);
+		JdbcTemplate template = new JdbcTemplate(connectionManager);
+		String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+		User user = new User(userId,"","","");
+		return template.execute(user,sql,"SELECT");
 	}
 
 	public void deleteAllUser() throws SQLException, PropertyVetoException {
+		JdbcTemplate template = new JdbcTemplate(connectionManager);
 		String sql = "DELETE FROM USERS";
-		execute(null,sql,QUERY_TYPE_DELETE);
-	}
-
-	private void close(Connection con, PreparedStatement pstmt)
-			throws SQLException {
-		if (pstmt != null) {
-			pstmt.close();
-		}
-		if (con != null) {
-			con.close();
-		}
+		template.execute(null,sql,"DELETE");
 	}
 
 	public Connection getConnection() throws SQLException, PropertyVetoException {
