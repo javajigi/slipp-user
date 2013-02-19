499176b631eef15e45c5e73023ba42e9c6d38b0a
kimmunsu <darkship1022@gmail.com>
2013-02-19 19:14:53 +0900

    update step2

--- src/main/java/net/slipp/dao/user/PreparedStatementSetter.java
+++
@@ -1,8 +0,0 @@
-package net.slipp.dao.user;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface PreparedStatementSetter {
-	void setValue(PreparedStatement pstmt) throws SQLException;
-}
--- src/main/java/net/slipp/dao/user/RowMapper.java
+++
@@ -1,8 +0,0 @@
-package net.slipp.dao.user;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface RowMapper<T> {
-	T setResultSet(ResultSet rs) throws SQLException;
-}
