0af34d7632a6602725e3273b8633ad21abe691bd
eclipse4j <eclipse4j@eclipse4j-PC>
2013-03-04 23:41:03 +0900

    repository

--- src/main/java/net/slipp/repository/user/UserRepository.java
+++ src/main/java/net/slipp/repository/user/UserRepository.java
@@ -2,8 +2,16 @@ package net.slipp.repository.user;
 
 import net.slipp.domain.user.User;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends CrudRepository<User, Long> {
 	public User findByUserId(String userId);
+
+	public User findByUserIdAndPassword(String userId, String password);
+	
+	@Modifying
+	@Query("update User o set o.password = :password where o.userId = :userId")
+	public void updatePassword(String password, String userId);
 }
