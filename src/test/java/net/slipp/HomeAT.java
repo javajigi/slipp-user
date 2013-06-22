3e558038dbc334dc2be212916fe005ff72934f72
javajigi <javajigi@gmail.com>
2013-06-22 17:05:23 +0900

    빌드, 테스트, 지속적 통합에 대한 강의 step4 - Acceptance Test

---
+++ src/test/java/net/slipp/HomeAT.java
@@ -0,0 +1,15 @@
+package net.slipp;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import net.slipp.support.AbstractBaseAT;
+
+import org.junit.Test;
+
+public class HomeAT extends AbstractBaseAT {
+	@Test
+	public void index() {
+		driver.get(DEFAULT_BASE_URL);
+		assertThat(driver.getTitle(), is("SLiPP"));
+	}
+}
