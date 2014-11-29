316051e27292875b84e18b233f74146820ee2ba7
parkjaesung <javajigi@gmail.com>
2014-11-29 20:38:03 +0900

    demo for fail

--- src/test/java/net/slipp/HomeAT.java
+++ src/test/java/net/slipp/HomeAT.java
@@ -10,6 +10,6 @@ public class HomeAT extends AbstractBaseAT {
 	@Test
 	public void index() {
 		driver.get(DEFAULT_BASE_URL);
-		assertThat(driver.getTitle(), is("SLiPP"));
+		assertThat(driver.getTitle(), is("SLiPP demo"));
 	}
 }
